048e4954bd6fe8c92c2648f92934025d6da10273
3untz <theclaymation@live.com>
2022-09-22 11:33:42 -0400

    modified windows built script

--- contrib/build-wine/build.sh
+++ contrib/build-wine/build.sh
@@ -63,8 +63,8 @@ $SUDO docker build -t $IMGNAME \
     || fail "Failed to create docker image"
 
 # This is the place where we checkout and put the exact revision we want to work
-# on. Docker will run mapping this directory to /homedir/wine/drive_c/electroncash
-# which inside wine will look like c:\electroncash.
+# on. Docker will run mapping this directory to /homedir/wine/drive_c/electronnovo
+# which inside wine will look like c:\electronnovo.
 FRESH_CLONE=`pwd`/contrib/build-wine/fresh_clone
 FRESH_CLONE_DIR="$FRESH_CLONE/$GIT_DIR_NAME"
 
@@ -86,9 +86,9 @@ FRESH_CLONE_DIR="$FRESH_CLONE/$GIT_DIR_NAME"
     -e BUILD_DEBUG="$BUILD_DEBUG" \
     -e PYI_SKIP_TAG="$PYI_SKIP_TAG" \
     --name ec-wine-builder-cont \
-    -v "$FRESH_CLONE_DIR":/homedir/wine/drive_c/electroncash:delegated \
+    -v "$FRESH_CLONE_DIR":/homedir/wine/drive_c/electronnovo:delegated \
     --rm \
-    --workdir /homedir/wine/drive_c/electroncash/contrib/build-wine \
+    --workdir /homedir/wine/drive_c/electronnovo/contrib/build-wine \
     $IMGNAME \
     ./_build.sh $REV
 ) || fail "Build inside docker container failed"
