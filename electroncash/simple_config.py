462106078ed4af5aa7deb05e92821f0715f0d3c7
garlix-testdev <testdev@garlix.io>
2023-05-10 02:58:45 +0100

    update fees again

--- electroncash/simple_config.py
+++ electroncash/simple_config.py
@@ -38,7 +38,7 @@ class SimpleConfig(PrintError):
         2. User configuration (in the user's config directory)
     They are taken in order (1. overrides config options set in 2.)
     """
-    fee_rates = [10e5 + x * 5e5 for x in range(0, 10)]
+    fee_rates = [2e5 + x * 1e5 for x in range(0, 9)]
 
     def __init__(self, options=None, read_user_config_function=None,
                  read_user_dir_function=None):
